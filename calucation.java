168d597e4530beb97c98d243aba6bed3b66251fb
Kadali Durga Prasad <161697177+Prasadkadali11@users.noreply.github.com>
2024-06-23 10:50:57 +0530

    Add files via upload

---
+++ calucation.java
@@ -0,0 +1,36 @@
+package polymorphism;
+
+public class calucation {
+	// method over loading
+
+	public void add(int a,int b) {
+		//add method code
+	}
+
+	public void add(int a,int b, int c) {
+		//add method code
+	}
+	public void add(double a,int b) {
+		//add method code
+	}
+	public void add(int a,double b) {
+		//add method code
+	}
+	public static void main(String[] args) {
+		calucation c = new calucation();
+		c.add(10, 61);
+		c.add(15, 16);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
